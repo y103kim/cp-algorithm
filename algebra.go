20d7b3119c94c07b573794856d5f1c41e56ee961
Youngwan Kim <y103.kim@gmail.com>
2021-06-13 14:43:17 +0900

    algebra: matrix power

--- algebra.go
+++ algebra.go
@@ -13,3 +13,42 @@ func pow(a, n int64) int64 {
 	}
 	return ret
 }
+
+// Matrix Power ==================================================================================
+
+const MOD uint64 = 1000000007
+const ORDER int = 8
+
+type Matrix [ORDER][ORDER]uint64
+
+func mul(a, b *Matrix) *Matrix {
+	z := &Matrix{}
+	for i := 0; i < ORDER; i++ {
+		for j := 0; j < ORDER; j++ {
+			for k := 0; k < ORDER; k++ {
+				z[i][j] = (z[i][j] + a[i][k]*b[k][j]) % MOD
+			}
+		}
+	}
+	return z
+}
+
+func getIdentity() *Matrix {
+	ret := &Matrix{}
+	for i := 0; i < ORDER; i++ {
+		ret[i][i] = 1
+	}
+	return ret
+}
+
+func matpow(a *Matrix, n uint64) *Matrix {
+	ret := getIdentity()
+	for n > 0 {
+		if n&1 != 0 {
+			ret = mul(ret, a)
+		}
+		a = mul(a, a)
+		n /= 2
+	}
+	return ret
+}
